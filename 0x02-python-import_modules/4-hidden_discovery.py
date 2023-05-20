77ad2d798abe41e9cf52eb33e183b9f4ee3c8e9b
somth-easy <somth_dope@yahoo.com>
2023-05-20 09:31:22 -0700

    Python modules

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,7 +2,9 @@
 
 if __name__ == "__main__":
     import hidden_4
-    names = dir(hidden_4)
-    for name in sorted(names):
+
+names = dir(hidden_4)
+
+for name in sorted(names):
     if name[:2] != "__":
         print(name)
