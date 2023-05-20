69dd4627d69a2ea7ca2f4a81ed1f04989f819983
somth-easy <somth_dope@yahoo.com>
2023-05-20 04:47:50 -0700

    Pyhton script

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,9 +9,8 @@ if count == 0:
 elif count == 1:
     print("1 argument:")
     for i in range(1, 2):
-        print("1: {}".format(sys.argv[i])) 
+        print("1: {}".format(sys.argv[i]))
 else:
     print("{} arguments:".format(count))
     for i in range(count):
         print("{}: {}".format(i + 1, sys.argv[i + 1]))
-
