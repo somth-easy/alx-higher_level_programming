05cbc845ccee08b9af2c67843750891800262b61
somth-easy <somth_dope@yahoo.com>
2023-05-20 04:32:40 -0700

    Pyhton script

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,6 +6,11 @@ if __name__ == "__main__":
 count = len(sys.argv) - 1
 if count == 0:
     print("0 arguments.")
+elif count == 1:
+    print("1: argument:")
+    for i in range(1, 2):
+        print("1: {}".format(sys.argv[i])) 
+
 else:
     print("{} arguments:".format(count))
     for i in range(count):
