8da6199afa1edc4d2f1ba6750086f361f899a601
somth-easy <somth_dope@yahoo.com>
2024-03-12 14:41:31 +0000

    unique 2 number combo

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,7 +3,7 @@
 for i in range(10):
     for n in range(i + 1, 10):
         if i == 8:
-            print("{}{}".format(i, n)
+            print("{}{}".format(i, n))
             continue
         else:
             print("{}{}".format(i, n), end=', ')
