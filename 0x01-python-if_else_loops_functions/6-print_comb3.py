2fbb2261af71b586af396a61e451c00f2f7d068f
somth-easy <somth_dope@yahoo.com>
2024-03-12 14:37:40 +0000

    unique 2 number combo

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+for i in range(10):
+    for n in range(i + 1, 10):
+        if i == 8:
+            print(f"{i}{n}")
+            continue
+        else:
+            print(f"{i}{n}", end=', ')
