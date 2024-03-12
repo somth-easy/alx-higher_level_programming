2c8996b0f40fbb283154b6c34a8d32c9665e9cbf
somth-easy <somth_dope@yahoo.com>
2024-03-12 08:19:43 +0000

    write to stderr

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -2,5 +2,6 @@
 import sys
 from datetime import datetime
 dt = datetime(2015, 10, 19)
-error_msg = f"and that piece of art is useful - Dora Korpar, {dt.strftime('%Y-%m-%d')}\n"
+error_msg = f"and that piece of art is useful - Dora Korpar, \
+        {dt.strftime('%Y-%m-%d')}\n"
 sys.stderr.write(error_msg)
