eb1c4fabd8b4623f44366ed47a619b79b91e571e
somth-easy <somth_dope@yahoo.com>
2024-07-04 15:38:22 +0000

    class with private attr

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""Defines a class Square."""
+
+
+class Square:
+    """Defines a class called square
+
+        Attributes:
+            size (DataFrame): private attribute of class Square
+    """
+
+    def __init__(self, size):
+        """Initialises the Square class
+
+            args:
+                size (DataFrame): value of size
+        """
+        self.__size = size
