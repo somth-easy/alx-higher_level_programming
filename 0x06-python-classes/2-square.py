63f77bbe828e9a12f2f84727a82fef34fa97a123
somth-easy <somth_dope@yahoo.com>
2024-07-04 21:02:22 +0000

    raise Errors

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""Defines a class Square."""
+
+
+class Square:
+    """Defines a class called square
+
+        Attributes:
+            size (DataFrame): private attribute of class Square
+    """
+
+    def __init__(self, size: int=0):
+        """Initialises the Square class
+
+            args:
+                size (DataFrame): value of size
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
