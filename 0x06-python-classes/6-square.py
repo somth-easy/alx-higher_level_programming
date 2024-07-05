15318c7fb56a3ec3ee256e4763f318f20401530a
somth-easy <somth_dope@yahoo.com>
2024-07-05 10:55:43 +0000

    Tuple

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -80,8 +80,7 @@ class Square:
         if self.__size == 0:
             print()
         else:
-            [print("") for i in range(0, self.__position[1])]
-            for i in range(0, self.__size):
-                [print(" ", end="") for j in range(0, self.__position[0])]
-                [print("#", end="") for k in range(0, self.__size)]
-                print("")
+            for y in range(0, self.position[1]):
+                print()
+            for _ in range(self.__size):
+                print(' ' * self.position[0] + '#' * self.size)
