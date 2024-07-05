5551220849c2ab039a51689665f47922f16d3b93
Biko <123925445+somth-easy@users.noreply.github.com>
2024-07-05 12:14:11 +0200

    Tuple

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -80,6 +80,9 @@ class Square:
         if self.__size == 0:
             print()
         else:
-            for _ in range(self.__size):
-                print(' ' * self.position[0] + '#' * self.size)
+            [print("") for i in range(0, self.__position[1])]
+            for i in range(0, self.__size):
+                [print(" ", end="") for j in range(0, self.__position[0])]
+                [print("#", end="") for k in range(0, self.__size)]
+                print("")
 
